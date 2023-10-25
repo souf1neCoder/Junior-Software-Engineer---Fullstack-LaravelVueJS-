0b48ae797020d3286dbf8bceaddfd13f6b4ebf4e
Soufiane M'channa <71340152+souf1neCoder@users.noreply.github.com>
2023-10-25 08:52:44 +0100

    add HasUuids trait to product model

--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
-    
+    use HasFactory,HasUuids;
+
     protected $fillable = ['name','description','price','image'];
 
     public function categories(){
