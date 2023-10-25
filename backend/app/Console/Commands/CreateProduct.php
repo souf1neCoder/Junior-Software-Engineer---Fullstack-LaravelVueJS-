b58aaf45cd39379f60c2aeaebf417f49b862f27a
Soufiane M'channa <71340152+souf1neCoder@users.noreply.github.com>
2023-10-25 04:28:43 +0100

    Command for create a Product

---
+++ backend/app/Console/Commands/CreateProduct.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+use App\Repositories\ProductsRepository;
+
+class CreateProduct extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'product:create {name : Product name} {description : Product description} {price : Product price} {categories* : Category IDs} {--image= : Image Src (optional)}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Product Creation';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(ProductsRepository $productsRepository)
+    {
+        try {
+            $name = $this->argument('name');
+            $description = $this->argument('description');
+            $price = $this->argument('price');
+            $imagePath = $this->option('image');
+            $categoryIds = $this->argument('categories');
+            $data = [
+                'name'=>$name,
+                'description'=>$description,
+                'price'=>$price,
+                'image'=>$imagePath,
+                'categories'=>$categoryIds
+            ];
+            $product = $productsRepository->store($data);
+
+
+            $this->info("Product created successfully ($product->name, $product->description, $product->price)");
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+            $this->error($th->getMessage());
+        }
+    }
+}
