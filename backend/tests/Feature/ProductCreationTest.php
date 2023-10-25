b790ed9346e6ee4f853d4647bc108cda4a855d51
Soufiane M'channa <71340152+souf1neCoder@users.noreply.github.com>
2023-10-25 04:29:40 +0100

    Cover product creation by a test

---
+++ backend/tests/Feature/ProductCreationTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProductCreationTest extends TestCase
+{
+    use WithFaker,RefreshDatabase;
+    /**
+     * A basic feature test example.
+     */
+    public function test_product_creation(): void
+    {
+        $data = [
+            'name' => $this->faker->name,
+            'description' => $this->faker->paragraph,
+            'price' => $this->faker->randomFloat(2, 10, 100),
+            'categories'=>[1,3],
+        ];
+
+        $response = $this->post('/api/products', $data);
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseHas('products', [
+            'name' => $data['name'],
+        ]);
+    }
+}
