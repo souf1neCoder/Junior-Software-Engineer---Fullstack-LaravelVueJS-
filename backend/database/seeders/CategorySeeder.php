2a47ccb0d7c8dffe06b2a5ac35e39e448d3c2100
Soufiane M'channa <71340152+souf1neCoder@users.noreply.github.com>
2023-10-25 09:26:49 +0100

    factories & seeders

---
+++ backend/database/seeders/CategorySeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            ['name' => 'Electronics', 'parent_id' => null],
+            ['name' => 'Clothing', 'parent_id' => null],
+            ['name' => 'Laptops', 'parent_id' => 1],
+            ['name' => 'Smartphones', 'parent_id' => 1],
+            ['name' => 'Men\'s Clothing', 'parent_id' => 2],
+            ['name' => 'Women\'s Clothing', 'parent_id' => 2]
+        ];
+
+        Category::insert($categories);
+    }
+}
---
+++ backend/database/seeders/ProductSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = Category::all()->pluck('id');
+        Product::factory(10)->create()->each(function($product) use($categories){
+            $product->categories()->attach($categories->random());
+        });
+    }
+}
